09ab62299c39f742542e437079891ea9ee774f52
Tavish Sharma <tavishsharma@Tavishs-MacBook-Pro.local>
2024-01-12 23:44:30 +0530

    commit for particles background setup

---
+++ src/components/particles/ParticlesBackground.js
@@ -0,0 +1,39 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import particlesConfig from "../../configs/ParticlesConfig";
+
+const ParticlesBackgroundObject = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => (particlesConfig),
+    [],
+  );
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <></>;
+};
+
+export default ParticlesBackgroundObject;
